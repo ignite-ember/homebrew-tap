931d04c66129ea45783976384ece4f61cba3ea4c
dmytrozezyk <dmytro.zezyk@vectorbridge.ai>
2026-04-20 12:09:22 +0200

    Initial formula

---
+++ Formula/ignite-ember.rb
@@ -0,0 +1,19 @@
+class IgniteEmber < Formula
+  include Language::Python::Virtualenv
+
+  desc "AI coding assistant with multi-agent orchestration"
+  homepage "https://ignite-ember.sh"
+  url "https://files.pythonhosted.org/packages/source/i/ignite-ember/ignite_ember-0.1.0.tar.gz"
+  sha256 "PLACEHOLDER"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "Ember Code", shell_output("#{bin}/ignite-ember --help")
+  end
+end
